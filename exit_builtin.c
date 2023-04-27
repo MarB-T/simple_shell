4970c6aad205ed30bf14040eca98966c5ada3a4d
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-04-27 13:06:15 -0700

    exit built-in

--- exit_builtin.c
+++ exit_builtin.c
@@ -13,7 +13,21 @@ void exit_bulitin(char **args)
 
 	if (args[i] != NULL)
 	{
-		stat = atoi(args[i]);
+		while (args[i] != '\0')
+		{
+			if (args[i] >= '0' && args[i] <= '9')
+			{
+				stat = stat * 10 + (args[i] - '0');
+			}
+			else
+			{
+				write(STDERR_FILENO, "exit", 22);
+				write(STDERR_FILENO, args[i], _strlen(args[i]));
+				write(STDERR_FILENO, "\n", 1);
+				free(args);
+				exit(2);
+			}
+		}
 	}
 	free(args);
 	exit(stat);
