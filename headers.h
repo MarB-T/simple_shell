b14ddcc057335faff91dd787dc74ddc8afba6000
MarB-T <danmarbet@gmail.com>
2023-04-29 05:02:53 +0300

    any pending file

--- headers.h
+++ headers.h
@@ -1,32 +1,127 @@
 #ifndef HEADERS_H
 #define HEADERS_H
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <stddef.h>
+#include <fcntl.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
+#define END_OF_FILE -2
+#define EXIT -3
 
 extern char **environ;
+char *name;
+int hist;
 
-int parsing_arg(char *inputstr, size_t num_of_chars, char ***argv);
-int process_command(char **argv);
-char *get_location(char *cmd);
-char *_getenv(const char *name);
-void shell_exit(void);
-void error_msg(int error_code);
-int _strlen(char *s);
-char *_strcat(char *dest, char *src);
-char *_strcpy(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-void *_realloc(void *old_mem, unsigned int nw, unsigned int old);
-void update_line_ptr(char **ptr, char *buf, size_t *n, ssize_t input);
-int env_bulitin(char **args);
-void exit_bulitin(char **args);
-char *_strtok(char *str, const char *delim);
+/**
+ * struct list_s - A new struct type defining a linked list.
+ * @dir: A directory path.
+ * @next: A pointer to another struct list_s.
+ */
+typedef struct list_s
+{
+	char *dir;
+	struct list_s *next;
+} list_t;
+
+/**
+ * struct builtin_s - A new struct type defining builtin commands.
+ * @name: The name of the builtin command.
+ * @f: A function pointer to the builtin command's function.
+ */
+typedef struct builtin_s
+{
+	char *name;
+	int (*f)(char **argv, char **front);
+} builtin_t;
+
+/**
+ * struct alias_s - A new struct defining aliases.
+ * @name: The name of the alias.
+ * @value: The value of the alias.
+ * @next: A pointer to another struct alias_s.
+ */
+typedef struct alias_s
+{
+	char *name;
+	char *value;
+	struct alias_s *next;
+} alias_t;
+
+alias_t *aliases;
+
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char **_strtok(char *line, char *delim);
+char *get_location(char *command);
+list_t *get_path_dir(char *path);
+int execute(char **args, char **front);
+void free_list(list_t *head);
+char *_itoa(int num);
 
+void handle_line(char **line, ssize_t read);
+void var_replacement(char **args, int *exe_ret);
+char *get_args(char *line, int *exe_ret);
+int call_args(char **args, char **front, int *exe_ret);
+int run_args(char **args, char **front, int *exe_ret);
+int handle_args(int *exe_ret);
+int check_args(char **args);
+void free_args(char **args, char **front);
+char **replace_aliases(char **args);
+
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
+int (*get_builtin(char *command))(char **args, char **front);
+int _strlen(const char *s);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+char *_strcpy(char *dest, const char *src);
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
+int s_exit(char **args, char **front);
+int s_env(char **args, char __attribute__((__unused__)) **front);
+int s_setenv(char **args, char __attribute__((__unused__)) **front);
+int s_unsetenv(char **args, char __attribute__((__unused__)) **front);
+int s_cd(char **args, char __attribute__((__unused__)) **front);
+int print_alias(char **args, char __attribute__((__unused__)) **front);
+int s_help(char **args, char __attribute__((__unused__)) **front);
+char **_copyenv(void);
+void free_env(void);
+char **_getenv(const char *var);
+char *get_args(char *line, int *exe_ret);
+int call_args(char **args, char **front, int *exe_ret);
+int run_args(char **args, char **front, int *exe_ret);
+int handle_args(int *exe_ret);
+int check_args(char **args);
+
+int create_error(char **args, int err);
+char *error_env(char **args);
+char *error_1(char **args);
+char *error_2_exit(char **args);
+char *error_2_cd(char **args);
+char *err_2_syn(char **args);
+char *error_126(char **args);
+char *error_127(char **args);
+
+alias_t *add_alias_end(alias_t **head, char *name, char *value);
+void free_alias_list(alias_t *head);
+list_t *add_node_end(list_t **head, char *dir);
+void free_list(list_t *head);
+
+void help_all(void);
+void help_alias(void);
+void help_cd(void);
+void help_exit(void);
+void help_help(void);
+void help_env(void);
+void help_setenv(void);
+void help_unsetenv(void);
+void help_history(void);
+
+int file_cmds(char *file_path, int *exe_ret);
 #endif
